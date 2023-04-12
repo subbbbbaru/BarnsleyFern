41e09e0678b51e8945c61a2d633ee1749ae16c16
subbbbbaru <saa2410@mail.ru>
2023-04-13 02:18:46 +1000

    some fix

--- fern/fern.go
+++ fern/fern.go
@@ -17,6 +17,7 @@ type Ferns struct {
 	Scale      float64
 	PlotHeight int
 	PlotWidth  int
+	gg         int
 }
 
 var Barnsley = [][]float64{{0, 0, 0, 0.16, 0, 0, 0.01},
@@ -60,44 +61,45 @@ const (
 )
 
 var (
-	xMinFrac   float64 = 0 // габариты фракталов
-	xMaxFrac   float64 = 0 // габариты фракталов
-	yMinFrac   float64 = 0 // габариты фракталов
-	yMaxFrac   float64 = 0 // габариты фракталов
-	probs              = make([]float64, 5)
-	affines            = [][]float64{} //make([][]float64, 4)
-	lenFractal         = 0
-	Points             = 100000
-	radius             = 0.69 // для SVG(поле Circle)
-	fractal            = [][]float64{}
-	points             = [][]int{}
+	xMinFrac float64 = 0 // габариты фракталов
+	xMaxFrac float64 = 0 // габариты фракталов
+	yMinFrac float64 = 0 // габариты фракталов
+	yMaxFrac float64 = 0 // габариты фракталов
+	// probs            = make([]float64, 5)
+	// affines = [][]float64{} //make([][]float64, 4)
+	//lenFractal         = 0
+	Points = 100000
+	radius = 0.69 // для SVG(поле Circle)
+	// fractal            = [][]float64{}
+	// points = [][]int{}
 )
 
-func (fern Ferns) initFractal() {
-	fractal = make([][]float64, fern.PointS)
+func (fern Ferns) initFractal() [][]float64 {
+	fractal := make([][]float64, fern.PointS)
 	for i := range fractal {
 		fractal[i] = make([]float64, 3)
 	}
+	return fractal
 }
-func (fern Ferns) initPoints() {
-	points = make([][]int, fern.PointS)
+func (fern Ferns) initPoints() [][]int {
+	points := make([][]int, fern.PointS)
 	for i := range points {
 		points[i] = make([]int, 2)
 	}
-
+	return points
 }
 
-func (fern *Ferns) makeFractal() {
+func (fern *Ferns) makeFractal(lenFractal *int) [][]float64 {
 
-	fern.initFractal()
+	fractal := fern.initFractal()
 
-	fern.makeMatrices()
+	probs, affines := fern.makeMatrices()
 	var options = len(probs) - 1
 	for i := 1; i < fern.PointS; i++ {
 		r := rand.Intn(101)
 		for j := 0; j < options; j++ {
 			if int(probs[j]*100) <= r && r < int(probs[j+1]*100) { // умножаю на 100 чтобы была не нулевая целая часть
-				transform(affines[j])
+				transform(affines[j], fractal, lenFractal)
 				fractal[i][2] = probs[j+1]
 				break
 			}
@@ -115,10 +117,12 @@ func (fern *Ferns) makeFractal() {
 			yMaxFrac = fractal[i][1]
 		}
 	}
+	return fractal
 }
 
-func (fern *Ferns) makeMatrices() {
-	affines = make([][]float64, 4)
+func (fern *Ferns) makeMatrices() ([]float64, [][]float64) {
+	probs := make([]float64, 5)
+	affines := make([][]float64, 4)
 
 	for i := 0; i < 4; i++ {
 		for j := 0; j < 6; j++ {
@@ -126,16 +130,20 @@ func (fern *Ferns) makeMatrices() {
 		}
 		probs[i+1] = probs[i] + fern.Fern[i][6]
 	}
+	return probs, affines
 }
 
-func transform(matrix []float64) {
-	lenFractal += 1
-	length := lenFractal
+func transform(matrix []float64, fractal [][]float64, lenFractal *int) {
+	*lenFractal += 1
+	length := *lenFractal
 	fractal[length][0] = (matrix[0] * fractal[length-1][0]) + (matrix[1] * fractal[length-1][1]) + (matrix[4])
 	fractal[length][1] = (matrix[2] * fractal[length-1][0]) + (matrix[3] * fractal[length-1][1]) + (matrix[5])
 }
 
-func (fern *Ferns) generatePoints() {
+func (fern *Ferns) generatePoints() [][]int {
+	lenFractal := 0
+	fractal := fern.makeFractal(&lenFractal)
+
 	fwidth := xMaxFrac - xMinFrac
 	fheight := yMaxFrac - yMinFrac
 	if int(fwidth) == 0 {
@@ -161,23 +169,19 @@ func (fern *Ferns) generatePoints() {
 		factor = yratio
 	}
 
-	f, _ := os.Create("coords.txt")
-	defer f.Close()
-
 	for k := 0; k < fern.PointS; k++ {
 		x := math.Round((fractal[k][0]-xmid)*float64(factor)*fern.Scale + float64(fern.PlotWidth)/2)
 		y := math.Round((fractal[k][1] - yMinFrac) * float64(factor) * fern.Scale)
 		if 0 <= x && x < float64(fern.PlotWidth) && 0 <= y && y < float64(fern.PlotHeight) {
 			if coords[int(x)][int(y)] == 0 {
-				fmt.Fprintln(f, int(x), int(y))
+
 				coords[int(x)][int(y)] = 1
 			} else {
 				coords[int(x)][int(y)]++
-				fmt.Fprintln(f, int(x), int(y))
 			}
 		}
 	}
-	fern.initPoints()
+	points := fern.initPoints()
 	for i := 0; i < len(coords); i++ {
 		k := len(coords[i])
 		if k == 0 {
@@ -191,12 +195,11 @@ func (fern *Ferns) generatePoints() {
 			}
 		}
 	}
+	return points
 }
 
 func (fern *Ferns) GenPng() {
-	lenFractal = 0
-	fern.makeFractal()
-	fern.generatePoints()
+	points := fern.generatePoints()
 
 	upLeft := image.Point{0, 0}
 	lowRight := image.Point{fern.PlotWidth, fern.PlotHeight}
@@ -215,9 +218,7 @@ func (fern *Ferns) GenPng() {
 }
 
 func (fern *Ferns) GenSvg() {
-	lenFractal = 0
-	fern.makeFractal()
-	fern.generatePoints()
+	points := fern.generatePoints()
 
 	f, _ := os.Create("fern.svg")
 	defer f.Close()
--- main.go
+++ main.go
@@ -1,18 +1,113 @@
 package main
 
 import (
+	"errors"
+	"flag"
+	"log"
 	"subbbbbaru/fern"
 )
 
-func main() {
-	myFern := &fern.Ferns{
-		Fern:       fern.Culcita,
-		Scale:      2,
-		PlotHeight: fern.PlotHeight,
-		PlotWidth:  fern.PlotWidth,
-		PointS:     fern.Points,
+var (
+	flagFern         *int
+	flagWidth        *int
+	flagHeight       *int
+	flagScale        *float64
+	flagPoints       *int
+	flagRenderFormat *int
+)
+
+const (
+	Barnsley = iota
+	Cyclosorus
+	Modified
+	Culcita
+	Fishbone
+	Tree
+	Bee
+	Your
+)
+
+func init() {
+	flagRenderFormat = flag.Int("f", 1, "Render format")
+	flagFern = flag.Int("fern", 1, "Format of fern")
+	flagScale = flag.Float64("s", 0.85, "Format of fern scale")
+	flagWidth = flag.Int("w", fern.PlotWidth, "Format of fern image width")
+	flagHeight = flag.Int("h", fern.PlotHeight, "Format of fern image height")
+	flagPoints = flag.Int("p", fern.Points, "Format of fern points")
+}
+
+func selectFern() (*[][]float64, error) {
+	switch *flagFern {
+	case Barnsley:
+		return &fern.Barnsley, nil
+		//myFern.Fern = fern.Barnsley
+	case Cyclosorus:
+		return &fern.Cyclosorus, nil
+		//myFern.Fern = fern.Cyclosorus
+	case Modified:
+		return &fern.Modified, nil
+		//myFern.Fern = fern.Modified
+	case Culcita:
+		return &fern.Culcita, nil
+		//myFern.Fern = fern.Culcita
+	case Fishbone:
+		return &fern.Fishbone, nil
+		//myFern.Fern = fern.Fishbone
+	case Tree:
+		return &fern.Tree, nil
+		//myFern.Fern = fern.Tree
+	case Bee:
+		return &fern.Bee, nil
+		//myFern.Fern = fern.Bee
+	case Your:
+		str := "Soon..."
+		return nil, errors.New(str)
 	}
-	// myFern.GenPngg()
-	// myFern.GenSvg()
-	myFern.GenPng()
+	return nil, errors.New("Wrong flag")
+}
+
+func selectScale() {
+	if *flagScale < 0 {
+		log.Panic("Scale is not positive")
+	}
+	if *flagPoints < 0 {
+		log.Panic("Points is not positive")
+	}
+}
+
+func selectSize() {
+	if *flagWidth < 0 {
+		log.Panic("Width is not positive")
+	}
+	if *flagHeight < 0 {
+		log.Panic("Height is not positive")
+	}
+}
+
+func main() {
+	flag.Parse()
+	mmF := &fern.Ferns{}
+	selectedFern, err := selectFern()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	mmF.Fern = *selectedFern
+
+	selectScale()
+	selectSize()
+
+	mmF.PlotHeight = *flagHeight
+	mmF.PlotWidth = *flagWidth
+	mmF.Scale = *flagScale
+	mmF.PointS = *flagPoints
+
+	switch *flagRenderFormat {
+	case 1:
+		mmF.GenPng()
+	case 2:
+		mmF.GenSvg()
+	default:
+		log.Panic("Error only 2 format(1: png, 2: svg)")
+	}
+
 }
